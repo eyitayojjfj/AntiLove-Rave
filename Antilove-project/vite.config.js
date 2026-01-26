d3f012e2565fb208e783de1540c12ba07b0985ca
eyitayojjfj <sulaimoneyitayo@gmail.com>
2026-01-27 00:33:17 +0100

    again

--- Antilove-project/vite.config.js
+++ Antilove-project/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   build: {
-  }
+  base: './',        // ensures relative paths
+  build: {
+    outDir: 'dist',  // default, but explicit is safe
+  },
 })
